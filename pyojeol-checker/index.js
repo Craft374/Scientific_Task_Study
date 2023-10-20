56455d3527ecec24a94ef927f306326f19df2b6f
Craft374 <leejeongwoo1103@gmail.com>
2023-10-20 09:02:27 +0900

    10.20

--- pyojeol-checker/index.js
+++ pyojeol-checker/index.js
@@ -1,11 +1,19 @@
 const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
 const createWindow = () => {
     const win = new BrowserWindow({
-      width: 1920,
-      height: 1080
+      width: 960,
+      height: 560,
+      fullscreen: false,
+      fullscreenable: false,
+      maximizable: false,
+      icon: path.join(__dirname, 'icon.ico'),
+      webPreferences: {
+        nodeIntegration: true
+      }
     });
-  
+    win.setMenu(null);
     win.loadFile('page/index.html');
   }
 
